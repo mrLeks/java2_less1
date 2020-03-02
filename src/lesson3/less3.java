8ae6e7636930b1495596f2c2c197fa348244188f
Aleksandr Kurzanov <mrleksit@gmail.com>
2020-03-02 15:22:36 +0300

    lesson3

---
+++ src/lesson3/less3.java
@@ -0,0 +1,34 @@
+package lesson3;
+
+
+import java.util.HashMap;
+
+//Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
+// Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
+// Посчитать сколько раз встречается каждое слово.
+public class less3 {
+    public static void main(String[] args){
+        String[] basketTeam = {"Lakers", "Grizzlies", "Bulls", "Lakers",
+                "CSKA", "Bulls", "Warriors", "Spurs", "Lakers", "Rockets"};
+        HashMap<String, Integer> team = new HashMap<>();
+        for(String x : basketTeam){
+            team.put(x, team.getOrDefault(x,0)+1);
+        }
+
+        System.out.println(team);
+
+        System.out.println("============================");
+
+        phonebook book = new phonebook();
+        book.add("Петрова","8912421");
+        book.add("Александров","8343322");
+        book.add("Сидорова","8876712");
+        book.add("Иванов","8912421");
+        book.add("Александров","811111111");
+        book.add("Петрова","8777777");
+
+        book.find("Петрова");
+        book.find("Иванов");
+        book.find("Александров");
+    }
+}
---
+++ src/lesson3/phonebook.java
@@ -0,0 +1,17 @@
+package lesson3;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class phonebook {
+    HashMap<String, HashSet<String>> pb = new HashMap<>();
+    public void add(String name, String number){
+        HashSet<String> book = pb.getOrDefault(name, new HashSet<>());
+        book.add(number);
+        pb.put(name, book);
+    }
+
+    public void find(String name){
+        System.out.println(name + "/Номер телефона: " + pb.getOrDefault(name, new HashSet<>()));
+    }
+}
